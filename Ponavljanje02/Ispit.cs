b5a44ae55b1b048449706a2c34219a4a0deebf76
AntonioMachiedo <antonio.machiedo@skole.hr>
2025-09-18 19:15:44 +0200

    nova klasa

---
+++ Ponavljanje02/Ispit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponavljanje02
+{
+    public class Ispit
+    {
+        Console.Write("Unesi naziv predmeta: ");
+        _predmet = Console.ReadLine();
+        Console.Write("Unesi  ime i prezime profesora koji predaje predmet: ");
+        _profesor = Console.ReadLine();
+        Console.Write("")
+    }
+}
